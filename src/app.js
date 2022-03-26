f6336da23d24c9afa90207f6b282c6d907e9f51a
Eric Cao <ericcao1999@hotmail.com>
2022-03-25 23:03:48 -0700

    Rewrote a code from lastweek and made it a function to be more useable in the future

--- src/app.js
+++ src/app.js
@@ -2,3 +2,27 @@
 function logfunc() {
     console.log(document.querySelector("div"))
 }
+
+//part 3
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+
+
+function avggradestud(new_list) {
+    const studentsScholarship = []
+    for (const st of new_list) {
+        st.grades = st.grades.reduce((a,b) => a+b,0) / st.grades.length; {
+            if(st.grades >= 80){
+                st.gradesavg = st.grades;
+                studentsScholarship.push(st)
+             }
+        }
+    }
+return studentsScholarship
+}
+
+console.log(avggradestud(students))
